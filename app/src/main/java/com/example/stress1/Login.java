5980744043ec68281222835d80cba33d0dd546bf
jfernando23 <jfernandoforero@ucundinamarca.edu.co>
2021-04-14 20:10:36 -0500

    Button

--- app/src/main/java/com/example/stress1/Login.java
+++ app/src/main/java/com/example/stress1/Login.java
@@ -1,12 +1,26 @@
 package com.example.stress1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    Button Registrop;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Registrop = (Button)findViewById(R.id.btnregistro);
+        Registrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                startActivity(i);
+            }
+        });
+
+
     }
 }
