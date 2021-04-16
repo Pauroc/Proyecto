1f65b584b365d41e266ba4be893660576d06c6b4
jfernando23 <jfernandoforero@ucundinamarca.edu.co>
2021-04-15 20:03:41 -0500

    Colores

--- app/src/main/java/com/example/stress1/Login.java
+++ app/src/main/java/com/example/stress1/Login.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class Login extends AppCompatActivity {
     Button Registrop;
     protected void onCreate(Bundle savedInstanceState) {
